e2b82bcf7c6454bc925a0918bcce553b9f22ecd1
Eduardo <temporariac432@gmail.com>
2025-02-24 09:20:32 -0300

    Correção de url

--- db/conn.js
+++ db/conn.js
@@ -11,7 +11,7 @@ async function main() {
     `mongodb://${process.env.DBUSER}:${process.env.DBPASS}@cluster0-shard-00-00.qbiqh.mongodb.net:27017,cluster0-shard-00-01.qbiqh.mongodb.net:27017,cluster0-shard-00-02.qbiqh.mongodb.net:27017/?ssl=true&replicaSet=atlas-10n4ir-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
   );
 
-  console.log("Conectado com sucesso!");
+  mongodb: console.log("Conectado com sucesso!");
 }
 
 module.exports = main;
