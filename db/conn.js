cd321b469ad484bcbd91047b65fec377bfbc13bf
Eduardo <temporariac432@gmail.com>
2025-02-24 09:41:04 -0300

    Informações de sistema corrigidas

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-
 require("dotenv").config();
 
 mongoose.set("strictQuery", true);
@@ -8,10 +7,9 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(
-    `mongodb://${process.env.DBUSER}:${process.env.DBPASS}@cluster0-shard-00-00.qbiqh.mongodb.net:27017,cluster0-shard-00-01.qbiqh.mongodb.net:27017,cluster0-shard-00-02.qbiqh.mongodb.net:27017/?ssl=true&replicaSet=atlas-10n4ir-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
+    "mongodb://eduardo:6Q7Qv96ULWi5T8ih@cluster0-shard-00-00.qbiqh.mongodb.net:27017,cluster0-shard-00-01.qbiqh.mongodb.net:27017,cluster0-shard-00-02.qbiqh.mongodb.net:27017/?ssl=true&replicaSet=atlas-10n4ir-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
   );
-
-  mongodb: console.log("Conectado com sucesso!");
+  console.log("Conectado com sucesso!");
 }
 
 module.exports = main;
